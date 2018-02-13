f0745b4f6e7d2390470f46d6c48e776f6e334dcf
marielacp <marielacp@outlook.com.pe>
2018-02-13 12:16:38 -0500

    añadiendo actualizaciones

---
+++ js/anim.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    setTimeout(function () {
+     /*   window.location.href = 'views/sobremi.html';*/
+    }, 4000);
+
+});
+$(function () {
+    setTimeout(function () {
+        $('.text-animation').removeClass('hidd');
+    }, 1000);
+})();
