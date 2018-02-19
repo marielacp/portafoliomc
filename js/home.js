7e1f4f318005f86f403737960326ea79ef7f8562
marielacp <marielacp@outlook.com.pe>
2018-02-18 21:31:00 -0500

    añadiendo actualizaciones

--- js/home.js
+++
@@ -1,8 +0,0 @@
- 
- $(window).scroll(function () {
-     if ($(this).scrollTop() > 300) /*height in pixels when the navbar becomes non opaque*/ {
-         $('.opaque-navbar').addClass('opaque');
-     } else {
-         $('.opaque-navbar').removeClass('opaque');
-     }
- });
